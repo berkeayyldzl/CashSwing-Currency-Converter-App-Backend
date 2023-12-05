f9fef5bfe82cd363358623a280daf6a8d974952b
berkeayyldzl <102878712+berkeayyldzl@users.noreply.github.com>
2023-12-05 15:59:50 +0300

    Add files via upload

---
+++ projBackendVol2/src/main/java/com/example/CurrencyExchangeController.java
@@ -0,0 +1,37 @@
+package com.example;
+//package com.example.demo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+	@RestController
+	@RequestMapping("/api/exchangerates")
+	public class CurrencyExchangeController {
+
+	// File: com.example.demo.CurrencyExchangeController.java
+	    @Autowired
+	    private CurrencyExchangeService currencyExchangeService;
+
+	    @GetMapping("/get/{baseCurrency}")
+	    public ExchangeRate getExchangeRates(@PathVariable String baseCurrency) {
+	        return currencyExchangeService.getExchangeRates(baseCurrency);
+	    }
+
+	    @GetMapping("/update/{baseCurrency}")
+	    public void updateExchangeRates(@PathVariable String baseCurrency) {
+	        currencyExchangeService.updateExchangeRates(baseCurrency);
+	    }
+	    
+	    
+	    //.save();
+	    //
+	    
+	}
+
+	
+	
+	
+
---
+++ projBackendVol2/src/main/java/com/example/ProjBackendVol2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjBackendVol2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjBackendVol2Application.class, args);
+	}
+
+}
